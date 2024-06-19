48ce95618d4cab3c3063cf2154d26882d1ad055f
Gustavo Starace <practicodecocina@gmail.com>
2024-06-19 11:29:55 +0200

    feat: color/logo

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,12 @@
 <template>
-    <nav class="bg-gray-800">
+    <div class="m-1 ">
+    <nav class="rounded-md p-1 bg-green-800">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                     <button type="button"
-                        class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        class="relative inline-flex items-center justify-center rounded-md p-2 text-green-400 hover:bg-green-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                         aria-controls="mobile-menu" aria-expanded="false">
                         <span class="absolute -inset-0.5"></span>
                         <span class="sr-only">Open main menu</span>
@@ -32,34 +33,34 @@
                 </div>
                 <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <div class="flex flex-shrink-0 items-center">
-                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        <img class="h-12 w-auto" src="../assets/biblio-logo.png"
                             alt="Your Company">
                     </div>
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
-                            <NuxtLink to="/" exact-active-class="bg-gray-900 text-white"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                            <NuxtLink to="/" exact-active-class="bg-green-900 text-white"
+                                class="rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-green-700 hover:text-white"
                                 aria-current="page">
                                 Home
                             </NuxtLink>
-                            <NuxtLink to="/books" exact-active-class="bg-gray-900 text-white"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                            <NuxtLink to="/books" exact-active-class="bg-green-900 text-white"
+                                class="rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                                 Libros
                             </NuxtLink>
-                            <NuxtLink to="/loans" exact-active-class="bg-gray-900 text-white"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                            <NuxtLink to="/loans" exact-active-class="bg-green-900 text-white"
+                                class="rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                                 Préstamos
                             </NuxtLink>
-                            <NuxtLink to="/users" exact-active-class="bg-gray-900 text-white"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                            <NuxtLink to="/users" exact-active-class="bg-green-900 text-white"
+                                class="rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                                 Usuarios
                             </NuxtLink>
-                            <NuxtLink to="/about" exact-active-class="bg-gray-900 text-white"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                            <NuxtLink to="/about" exact-active-class="bg-green-900 text-white"
+                                class="rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                                 Nosotros
                             </NuxtLink>
-                            <NuxtLink to="/contact" exact-active-class="bg-gray-900 text-white"
-                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                            <NuxtLink to="/contact" exact-active-class="bg-green-900 text-white"
+                                class="rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                                 Contacto
                             </NuxtLink>
                         </div>
@@ -67,7 +68,7 @@
                 </div>
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <button type="button"
-                        class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        class="relative rounded-full bg-green-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-green-800">
                         <span class="absolute -inset-1.5"></span>
                         <span class="sr-only">View notifications</span>
                         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
@@ -81,7 +82,7 @@
                     <div class="hidden relative ml-3">
                         <div>
                             <button type="button"
-                                class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                class="relative flex rounded-full bg-green-800 text-md focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
@@ -104,11 +105,11 @@
                         <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                            <a href="#" class="block px-4 py-2 text-md text-green-700" role="menuitem" tabindex="-1"
                                 id="user-menu-item-0">Perfil</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                            <a href="#" class="block px-4 py-2 text-md text-green-700" role="menuitem" tabindex="-1"
                                 id="user-menu-item-1">Ajustes</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1"
+                            <a href="#" class="block px-4 py-2 text-md text-green-700" role="menuitem" tabindex="-1"
                                 id="user-menu-item-2">Salir</a>
                         </div>
                     </div>
@@ -119,32 +120,33 @@
         <!-- Mobile menu, show/hide based on menu state. -->
         <div class="sm:hidden" id="mobile-menu">
             <div class="space-y-1 px-2 pb-3 pt-2">
-                <NuxtLink to="/" exact-active-class="bg-gray-900 text-white"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                <NuxtLink to="/" exact-active-class="bg-green-900 text-white"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-green-700 hover:text-white"
                     aria-current="page">
                     Home
                 </NuxtLink>
-                <NuxtLink to="/books" exact-active-class="bg-gray-900 text-white"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                <NuxtLink to="/books" exact-active-class="bg-green-900 text-white"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                     Libros
                 </NuxtLink>
-                <NuxtLink to="/loans" exact-active-class="bg-gray-900 text-white"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                <NuxtLink to="/loans" exact-active-class="bg-green-900 text-white"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                     Préstamos
                 </NuxtLink>
-                <NuxtLink to="/users" exact-active-class="bg-gray-900 text-white"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                <NuxtLink to="/users" exact-active-class="bg-green-900 text-white"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                     Usuarios
                 </NuxtLink>
-                <NuxtLink to="/about" exact-active-class="bg-gray-900 text-white"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                <NuxtLink to="/about" exact-active-class="bg-green-900 text-white"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                     Nosotros
                 </NuxtLink>
-                <NuxtLink to="/contact" exact-active-class="bg-gray-900 text-white"
-                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                <NuxtLink to="/contact" exact-active-class="bg-green-900 text-white"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-green-700 hover:text-white">
                     Contacto
                 </NuxtLink>
             </div>
         </div>
     </nav>
+</div>
 </template>
--- components/User.vue
+++ components/User.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="m4 p-4 border rounded-md shadow-lg bg-white flex flex-col">
-        <h1>Id: 1</h1>
-        <h2>Nombre: Gustavo</h2>
-        <h3>Apellido Starace</h3>
-        <h4>Correo Electrónico: kaaslange@hotmail.com</h4>
+    <div class="m-4 border">
+    <div class="p-4 border rounded-md shadow-lg bg-white flex flex-col">
+        <h1 >Id: 1</h1>
+        <h2 class="text-l font-bold">Gustavo Starace</h2>
+        <h4 class="text-gray-500 hidden md:block">Correo Electrónico: kaaslange@hotmail.com</h4>
         <h4>Es Administrador: Si</h4>
-        <button>Editar</button><button>Cerrar</button>
+        <button class="bg-green-500 text-white px-4 py-1 mt-2 rounded-lg hover:bg-green-700 shadow-lg">Editar</button>
     </div>
+</div>
 </template>
